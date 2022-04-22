8a5c1ec911dbb5e040f5ae3af438a5ac4d8d1398
Ken Zhang <wz840@nyu.edu>
2022-04-22 21:31:05 +0800

    Merge bond and equity data to calculate all premiums

--- double_low_strategy.py
+++ double_low_strategy.py
@@ -36,8 +36,9 @@ def read_csv(file, date_col, index_list, data_type_map, col_used=[], clean_funct
     # for code, group in groups:
     #     print('Bode Symbol: ' + str(code))
     #     print(group.first())
-    data_frame.reset_index(inplace=True)
-    data_frame.set_index(index_list, inplace=True)
+    if index_list:
+        data_frame.reset_index(inplace=True)
+        data_frame.set_index(index_list, inplace=True)
     if clean_function:
         clean_function(data_frame)
     return data_frame
@@ -51,7 +52,7 @@ def read_excel(file, data_type_map, clean_function=None):
     :param clean_function: the data cleansing function, default None - No cleansing needed
     :return:
     """
-    data_framework = pd.read_excel(file, dtype=data_type_map, header=0, index_col=0)
+    data_framework = pd.read_excel(file, dtype=data_type_map, header=0)
     if clean_function:
         clean_function(data_framework)
     return data_framework
@@ -82,16 +83,31 @@ def complete_with_market(ori_code, market):
             return ori_code + 'SZ'
 
 
-def get_mapping(df):
-    return df['EQUITY_SECUCODE'].to_dict()
+def get_mapping(df: pd.DataFrame):
+    mapping = {}
+    for index, row in df.iterrows():
+        mapping[row['SECUCODE']] = row['EQUITY_SECUCODE']
+    return mapping
 
 
-def calculate_premium(bond_df: pd.DataFrame, bond_equity_mapping: pd.DataFrame, equity_df: pd.DataFrame, face_value: int):
-    symbol_map = bond_equity_mapping['EQUITY_SECUCODE'].to_dict()
-    bond_df['Bond Premium'] = bond_df.apply(lambda row: Strategy.calculate_bond_premium(row['CLOSE'], face_value), axis=1)
-    bond_df['Convert Premium'] = bond_df.apply(lambda row: Strategy.calculate_convert_premium(row['CLOSE'], row['CONVERTIBLE_PX'],
+def calculate_premium(bond_df: pd.DataFrame, bond_equity_mapping: pd.DataFrame, equity_df: pd.DataFrame, face_value=100):
+    """
+    Calculate premium of bond and equity
+    :param bond_df: bond data with end date (baseline as corresponding equity)
+    :param bond_equity_mapping:  bond and equity symbol mapping
+    :param equity_df: equity data with start date (baseline as corresponding bond)
+    :param face_value: face value of bond - default=100
+    :return: consolidated data frame with all calculated premium for all trading days
+    """
+    # merge two dfs to collect stock symbol and adjusted close price for a particular trading day
+    bond_df = pd.merge(bond_df, bond_equity_mapping.loc[:, ['SECUCODE', 'EQUITY_SECUCODE']], how='left', on='SECUCODE')
+    bond_df = pd.merge(bond_df, equity_df, how='left', left_on=['EQUITY_SECUCODE', 'TRADEDATE'], right_on=['S_INFO_WINDCODE', 'TRADE_DT'])
+
+    # calculate premiums based on bond/equity close price, convertible strike price and face value
+    bond_df['BOND_PREMIUM'] = bond_df.apply(lambda row: Strategy.calculate_bond_premium(row['CLOSE'], face_value), axis=1)
+    bond_df['CONVERT_PREMIUM'] = bond_df.apply(lambda row: Strategy.calculate_convert_premium(row['CLOSE'], row['S_DQ_CLOSE'],
                                                                                               row['CONVERTIBLE_PX'], face_value), axis=1)
-    bond_df['Premium'] = bond_df.apply(lambda row: row['Bond Premium'] + row['Convert Premium'], axis=1)
+    bond_df['PREMIUM_INDEX'] = bond_df.apply(lambda row: format(row['BOND_PREMIUM'] + row['CONVERT_PREMIUM'], '.2%'), axis=1)
     return bond_df
 
 
@@ -99,23 +115,38 @@ def output_results(df: pd.DataFrame):
     if not os.path.exists('results'):
         os.makedirs('results')
     path = 'results/premium.csv'
-    df.to_csv(path, index_label='TRADEDATE', header=True)
+    df.to_csv(path, header=True)
     return path
 
 
+def truncate_by_date(filter_function, df: pd.DataFrame):
+    return df[(filter_function(df))]
+
+
+def start_date_filter(date_str: str, date_col: str):
+    s_date = datetime.datetime.strptime(date_str, '%Y%m%d').date()
+    return lambda x: x[date_col].dt.date >= s_date
+
+
+def end_date_filter(date_str: str, date_col: str):
+    e_date = datetime.datetime.strptime(date_str, '%Y%m%d').date()
+    return lambda x: x[date_col].dt.date <= e_date
+
+
 if __name__ == '__main__':
-    cb_df = read_csv('data/cbData.csv',  # file path
-                     'TRADEDATE',  # date column
-                     ['SECUCODE', 'TRADEDATE'],  # compound index column
-                     {'SECUCODE': str, 'TRADEDATE': str})  # column data type
+    cb_df = truncate_by_date(end_date_filter('20201009', 'TRADEDATE'), read_csv('data/cbData.csv',  # file path
+                                                                                'TRADEDATE',  # date column
+                                                                                None,  # compound index column
+                                                                                {'SECUCODE': str, 'TRADEDATE': str}))  # column data type
     mapping_df = read_excel('data/announcement.xlsx',
                             {'SECUCODE': str, 'SECUMARKET': int, 'EQUITY_SECUCODE': str, 'EQUITY_SECUMARKET': int},  # column data type
                             mapping_cleansing)  # data cleansing
-    stock_df = read_csv('D:/OneDrive/SAIF/2022Spring/IFL-Stock/Wind数据/AShareEODPrices.csv',  # file path
-                        'TRADE_DT',  # date column
-                        ['S_INFO_WINDCODE', 'TRADE_DT'],  # compound index column
-                        {'S_INFO_WINDCODE': str, 'TRADE_DT': str},  # column data type
-                        ['S_INFO_WINDCODE', 'TRADE_DT', 'S_DQ_ADJCLOSE'])  # column needed
-    premium_df = calculate_premium(cb_df, mapping_df, stock_df, 100)
-    # output_results(premium_df)
+    stock_df = truncate_by_date(start_date_filter('20121231', 'TRADE_DT'),
+                                read_csv('D:/OneDrive/SAIF/2022Spring/IFL-Stock/Wind数据/AShareEODPrices.csv',  # file path
+                                         'TRADE_DT',  # date column
+                                         None,  # compound index column
+                                         {'S_INFO_WINDCODE': str, 'TRADE_DT': str},  # column data type
+                                         ['S_INFO_WINDCODE', 'TRADE_DT', 'S_DQ_CLOSE']))  # column needed
+    premium_df = calculate_premium(cb_df, mapping_df, stock_df)
+    output_results(premium_df)
     print(mapping_df.head(5))
